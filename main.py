0544d514fb8ccc42cb7c55967d40cfc09fdf476d
Beknazar Abdyldaev <abdyldaev.duff@gmail.com>
2022-12-07 21:29:38 +0600

    create connection logic

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        if len(self.active_connections) >= 2:
+            await websocket.accept()
+            await websocket.close(4000)
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, data: str):
+        for connection in self.active_connections:
+            await connection.send_json(data)
+
+
+manager = ConnectionManager()
+
+
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+    
+    await manager.connect(websocket)
+    
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await websocket.send_text(f"Message text was: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+    except:
+        pass
