99c7f329ef404ed0f4c2d8d96c68244525bf2402
Beknazar Abdyldaev <abdyldaev.duff@gmail.com>
2022-12-08 13:35:12 +0600

    add server and client logic

--- main.py
+++ main.py
@@ -1,29 +1,110 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from typing import List
+from fastapi import FastAPI, WebSocket, status
+from starlette.websockets import WebSocketDisconnect
+import json
 
-from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
+def init_board():
+    # create empty board
+    return [
+        None, None, None,
+        None, None, None,
+        None, None, None,
+    ]
+
+
+board = init_board()
+
+
+def is_draw():
+    # checks if a draw
+    global board
+    for cell in board:
+        if not cell:
+            return False
+    board = init_board()
+    return True
+
+
+def if_won():
+    # check if some player has just won the game
+    global board
+    if board[0] == board[1] == board[2] != None or \
+            board[3] == board[4] == board[5] != None or \
+            board[6] == board[7] == board[8] != None or \
+            board[0] == board[3] == board[6] != None or \
+            board[1] == board[4] == board[7] != None or \
+            board[2] == board[5] == board[8] != None or \
+            board[0] == board[4] == board[8] != None or \
+            board[6] == board[4] == board[2] != None:
+        board = init_board()
+        return True
+    return False
+
+
+async def update_board(manager, data):
+    ind = int(data['cell']) - 1
+    data['init'] = False
+    if not board[ind]:
+        # cell is empty
+        board[ind] = data['player']
+        if is_draw():
+            data['message'] = "draw"
+        elif if_won():
+            data['message'] = "won"
+        else:
+            data['message'] = "move"
+    else:
+        data['message'] = "choose another one"
+    await manager.broadcast(data)
+    if data['message'] in ['draw', 'won']:
+        manager.connections = []
+
+
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: list[WebSocket] = []
+        self.connections: List[WebSocket] = []
 
     async def connect(self, websocket: WebSocket):
-        if len(self.active_connections) >= 2:
+        # dealing with incomming connections here
+        if len(self.connections) >= 2:
+            # denies connection for 3rd player
             await websocket.accept()
             await websocket.close(4000)
-        await websocket.accept()
-        self.active_connections.append(websocket)
+        else:
+            await websocket.accept()
+            # adding the connections to the connection's list
+            self.connections.append(websocket)
+            if len(self.connections) == 1:
+                # the first connected persone plays by X and should wait for a second player
+                await websocket.send_json({
+                    'init': True,
+                    'player': 'X',
+                    'message': 'Waiting for another player',
+                })
+            else:
+                # the second player plays by O
+                await websocket.send_json({
+                    'init': True,
+                    'player': 'O',
+                    'message': '',
+                })
+                # signals to the first player that the second player has just connected 
+                await self.connections[0].send_json({
+                    'init': True,
+                    'player': 'X',
+                    'message': 'Your turn!',
+                })
 
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
-
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
+    async def disconnect(self, websocket: WebSocket):
+        self.connections.remove(websocket)
 
     async def broadcast(self, data: str):
-        for connection in self.active_connections:
+        # broadcasting data to all connected clients
+        for connection in self.connections:
             await connection.send_json(data)
 
 
@@ -32,13 +113,14 @@ manager = ConnectionManager()
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    
     await manager.connect(websocket)
-    
     try:
         while True:
+            # here we are waiting for an oncomming message from clients
             data = await websocket.receive_text()
-            await websocket.send_text(f"Message text was: {data}")
+            data = json.loads(data)
+            # precessing the incomming message
+            await update_board(manager, data)
     except WebSocketDisconnect:
         manager.disconnect(websocket)
     except:
